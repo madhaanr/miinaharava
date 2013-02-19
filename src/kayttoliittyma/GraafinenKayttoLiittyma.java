1da0666edb6332c995144b43d46bec50fef949a9
mhaanran <mhaanran@dhp-18.cs.helsinki.fi>
2013-02-19 16:41:41 +0200

    Graafisenkäyttöliittymän tekoa.

--- src/kayttoliittyma/GraafinenKayttoLiittyma.java
+++ src/kayttoliittyma/GraafinenKayttoLiittyma.java
@@ -1,5 +1,6 @@
 package kayttoliittyma;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -29,55 +30,27 @@ public class GraafinenKayttoLiittyma extends JFrame implements ActionListener {
     private long alkuAika;
     JButton[][] miinaNappi;
     
+    
     public GraafinenKayttoLiittyma() {
+        
+    }   
+    public void alustaKomponentit() {  
         miinaKentta = new PeliLogiikka(kentanKoko);
         miinaKentta.miinojaLahella();
-    }   
-    public void alustaKomponentit() {
         setTitle("Miinaharava");
-        setPreferredSize(new Dimension(700,500));  
+        setPreferredSize(new Dimension(500,500));  
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
         GridBagLayout gridBagLayout = new GridBagLayout();      
         paaIkkuna = new JPanel();
         paaIkkuna.setLayout(gridBagLayout);   
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
-        uusiPeli = new JButton("Uusi Peli");
-        gridBagConstraints.fill=GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.weightx=0.1;
-        gridBagConstraints.gridy=0;
-        gridBagConstraints.gridx=0;
-        paaIkkuna.add(uusiPeli,gridBagConstraints);
-        uusiPeli.addActionListener(this);
         
-        naytaAikaa = new JLabel();
-        gridBagConstraints.fill=GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.weightx=0.5;
-        gridBagConstraints.gridy=0;
-        gridBagConstraints.gridx=kentanKoko-1;
-        naytaAikaa.setText(ajanMuoto.format(new Date(0)));
-        naytaAikaa.setFont(new Font("Dialog",Font.PLAIN, 20));
-        timer = new Timer(1000,this);
-        timer.setRepeats(true);
-        alkuAika=System.currentTimeMillis();
-        timer.start();
-        paaIkkuna.add(naytaAikaa,gridBagConstraints);
+        uusiPeli(gridBagConstraints);
         
-        miinaNappi=new JButton[kentanKoko][kentanKoko];
-        gridBagConstraints.fill = GridBagConstraints.BOTH;
-        gridBagConstraints.weightx=0.1;
-        gridBagConstraints.weighty=0.1;
-        gridBagConstraints.gridy=1;
-        gridBagConstraints.gridx=0;
-        for(int i=0;i<kentanKoko;++i) {
-            for(int j=0;j<kentanKoko;++j) {       
-                miinaNappi[i][j]=new JButton();
-                miinaNappi[i][j].addActionListener(this);
-                paaIkkuna.add(miinaNappi[i][j],gridBagConstraints);
-                gridBagConstraints.gridx=0+j;
-            }
-            gridBagConstraints.gridy=1+i;        
-        }
+        naytaAikaa(gridBagConstraints);
+        
+        miinaNappi(gridBagConstraints);
               
         setContentPane(paaIkkuna);
         setResizable(false);
@@ -93,8 +66,57 @@ public class GraafinenKayttoLiittyma extends JFrame implements ActionListener {
         if(e.getSource().equals(uusiPeli)) {
             alustaKomponentit();
         }
-        else {
+        if(e.getSource() instanceof JButton) {
+            e.getActionCommand();
             miinaKentta.onkoMiina(koordinaattiX, koordinaattiY);
+        }
+         
+    }
+
+    public void uusiPeli(GridBagConstraints gridBagConstraints) {
+        uusiPeli = new JButton("Uusi Peli");
+        gridBagConstraints.fill=GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx=0.0;
+        gridBagConstraints.gridy=0;
+        gridBagConstraints.gridx=0;
+        paaIkkuna.add(uusiPeli,gridBagConstraints);
+        uusiPeli.addActionListener(this);
+    }
+
+    public void naytaAikaa(GridBagConstraints gridBagConstraints) {
+        naytaAikaa = new JLabel();
+        gridBagConstraints.fill=GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx=0.0;
+        gridBagConstraints.gridy=0;
+        gridBagConstraints.gridx=kentanKoko-1;
+        naytaAikaa.setText(ajanMuoto.format(new Date(0)));
+        naytaAikaa.setFont(new Font("Dialog",Font.PLAIN, 20));
+        timer = new Timer(1000,this);
+        timer.setRepeats(true);
+        alkuAika=System.currentTimeMillis();
+        timer.start();
+        paaIkkuna.add(naytaAikaa,gridBagConstraints);
+    }
+
+    public void miinaNappi(GridBagConstraints gridBagConstraints) {
+        miinaNappi=new JButton[kentanKoko][kentanKoko];
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.weightx=0.1;
+        gridBagConstraints.weighty=1.0;
+        gridBagConstraints.gridy=1;
+        gridBagConstraints.gridx=0;
+        gridBagConstraints.ipadx=0;
+        gridBagConstraints.ipady=0;
+//        gridBagConstraints.gridwidth=1;
+        for(int i=0;i<kentanKoko;++i) {
+            for(int j=0;j<kentanKoko;++j) {       
+                miinaNappi[i][j]=new JButton();
+                miinaNappi[i][j].addActionListener(this);
+//                miinaNappi[i][j].setActionCommand("command"+i+"-"+j);
+                paaIkkuna.add(miinaNappi[i][j],gridBagConstraints);
+                gridBagConstraints.gridx=0+j;
+            }
+            gridBagConstraints.gridy=1+i;        
         }
     }
 }
